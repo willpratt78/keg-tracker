8df1fb9545e3a7ceeb630d21b8fea72857e2705f
William Pratt <willpratt78@gmail.com>
2022-07-01 11:38:14 -0700

    WIP lunch

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types"
 
-functin Keg(){
+function Keg(props){
   return(
     <React.Fragment>
-
+      <h3>{props.name} by {props.brand}</h3>
+      <p>{props.price}</p>
+      <p>{props.alcoholContent}</p>
     </React.Fragment>
   );
 }
 
-export default Ticket;
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  alcoholContent: PropTypes.string
+}
+
+export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -3,7 +3,11 @@ import Keg from "./Keg";
 
 function KegList(){
   return (
-    <Keg />
+    <Keg 
+    name = "PBR"
+    brand = "will's"
+    price = "8.99"
+    alcoholContent = "2%" />
   );
 }
 
