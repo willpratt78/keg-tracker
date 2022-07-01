e73960b8b7551190c8863693b1dd35e355c24328
William Pratt <willpratt78@gmail.com>
2022-07-01 15:34:42 -0700

    update to KegLIst

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Keg from "./Keg";
 
-function KegList(){
+
+function KegList(props){
   return (
-    <Keg 
-    name = "PBR"
-    brand = "will's"
-    price = "8.99"
-    alcoholContent = "2%" />
+    <React.Fragment>  
+      <hr/>
+      {props.kegList.map((keg, index) =>
+        <Ticket names ={keg.names}
+        location={keg.location}
+        issue={keg.issue}
+        key={index}/>
+      )}
+    </React.Fragment>
   );
 }
+
+// Add propTypes for ticketList.
+KegList.protoTypes = {
+  kegList: PropTypes.array
+};
 
 export default KegList;
 
